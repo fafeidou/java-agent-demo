cbd26e047fe7f58d68611c222f00ccc1fb31695a
qinfuxiang <17521222912@163.com>
2020-07-06 09:44:52 +0800

    Initial commit

---
+++ src/main/java/com/java/agent/demo/JavaAgentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java.agent.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAgentDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAgentDemoApplication.class, args);
+    }
+
+}
